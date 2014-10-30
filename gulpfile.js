6a89b62d09ab2bb8b016591cdcb4dafdf007a04b
Felix Zandanel <felix@zandanel.me>
2014-10-30 18:49:26 +0100

    Add image minification task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var fs = require('fs');
+var eventStream = require('event-stream');
 var gulp = require('gulp');
+var newer = require('gulp-newer');
+var notify = require("gulp-notify");
 var addsrc = require('gulp-add-src');
 var bower = require('gulp-bower');
 var autoprefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var spritesmith = require('gulp.spritesmith');
+var imagemin = require('gulp-imagemin');
 var cssmin = require('gulp-cssmin');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
@@ -12,12 +17,15 @@ var watch = require('gulp-watch');
 
 // Setup directories to work with
 var dirs = {
+  build: 'build',
   src: {
+    images: 'source/images',
     bower: 'bower_components',
     js: 'source/javascripts',
     sass: 'source/styles'
   },
   dest: {
+    images: 'images',
     js: 'js',
     css: 'css'
   }
@@ -41,13 +49,70 @@ gulp.task('bower', function () {
   });
 });
 
+// Generate sprites, copy over other images & minify them all
+gulp.task('images', function () {
+  var cssVarMapper = function (sprite) {
+    sprite.name = 'sprite-' + sprite.name.replace(/@.*$/, '');
+  };
+
+  // @1x
+  var sprite1x = gulp.src(dirs.src.images + '/sprite/*@1x.png')
+    .pipe(spritesmith({
+      imgName: 'sprite@1x.png',
+      cssName: 'sprite@1x.scss',
+      cssVarMap: cssVarMapper
+    }));
+
+  // @2x
+  var sprite2x = gulp.src(dirs.src.images + '/sprite/*@2x.png')
+    .pipe(spritesmith({
+      imgName: 'sprite@2x.png',
+      cssName: 'sprite@2x.scss',
+      cssVarMap: cssVarMapper
+    }));
+
+  // @3x
+  var sprite3x = gulp.src(dirs.src.images + '/sprite/*@3x.png')
+    .pipe(spritesmith({
+      imgName: 'sprite@3x.png',
+      cssName: 'sprite@3x.scss',
+      cssVarMap: cssVarMapper
+    }));
+
+  // Init merged stream with other images
+  var mergedImageStream = gulp.src(dirs.src.images + '/*.*');
+
+  [sprite1x, sprite2x, sprite3x].forEach(function (sprite) {
+    mergedImageStream = eventStream.merge(mergedImageStream, sprite.img);
+
+    // Note: This is actually a SCSS file...
+    sprite.css && sprite.css
+      .pipe(gulp.dest(dirs.build + '/sprite-sass'));
+  });
+
+  mergedImageStream
+    .pipe(imagemin({
+      optimizationLevel: 4,
+      progressive: true,
+      use: [
+        require('imagemin-advpng')({optimizationLevel: 4}),
+        require('imagemin-pngcrush')({reduce: true})
+      ]
+    }))
+    .on('error', notify.onError({
+      message: "Please check for correct file extensions and file corruption. (Error: <%= error.message %>)",
+      title: "Error during image minification"
+    }))
+    .pipe(gulp.dest(dirs.dest.images));
+});
+
 // Build CSS from SASS
 gulp.task('css', function () {
-  gulp.src(dirs.src.sass + '/main.scss')
+  gulp.src([dirs.src.sass + '/main.scss'])
     .pipe(sass({
-      onError: function (err) {
-        return notify().write(err);
-      }
+      onError: notify.onError(function (error) {
+        return error.message;
+      })
     }))
     .pipe(autoprefixer('last 2 version', 'ie 9'))
     .pipe(concat('app.css'))
@@ -75,10 +140,12 @@ gulp.task('js', function () {
 gulp.task('watch', function () {
   gulp.watch(dirs.src.sass + '/**/*.scss', ['css']);
   gulp.watch(dirs.src.js + '/**/*.js', ['js']);
+  gulp.watch(dirs.src.images + '/**/*', ['images'])
 });
 
 gulp.task('build', [
   'clean',
+  'images',
   'bower',
   'css',
   'js'
