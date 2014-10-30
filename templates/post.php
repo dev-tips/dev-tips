4b4d027294c2d0a41e4b924b943c5648bf9eeae8
Thomas Rasshofer <me@thomasrasshofer.com>
2014-10-30 09:15:06 +0100

    Add GitHub usernames as meta data for every post

--- templates/post.php
+++ templates/post.php
@@ -5,7 +5,13 @@
         <div itemprop="articleBody">
             <?php text($page->text()) ?>
         </div>
-        <a href="<?php echo $page->url() ?>" class="permalink"><time datetime="<?php echo date('c', $page->date()) ?>" itemprop="datePublished"><?php echo date('M j', $page->date()) ?><span><?php echo date('S', $page->date()) ?></span> <?php echo date('Y', $page->date()) ?></time></a>
+        <div class="meta">
+            <a href="<?php echo $page->url() ?>" class="permalink"><time datetime="<?php echo date('c', $page->date()) ?>" itemprop="datePublished"><?php echo date('M j', $page->date()) ?><span><?php echo date('S', $page->date()) ?></span> <?php echo date('Y', $page->date()) ?></time></a>
+            ·
+            <?php foreach (explode(',', $page->contributors()) as $contributor): ?>
+            <a href="https://github.com/<?php echo trim($contributor) ?>" target="_blank" class="github-contributor"><span class="github-username"><?php echo trim($contributor) ?></span></a>
+            <?php endforeach ?>
+        </div>
     </div>
 </article>
 <?php template('footer') ?>
