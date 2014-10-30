eedb63be71f0ec8b403f84bc76dbbd24823141a2
Thomas Rasshofer <me@thomasrasshofer.com>
2014-10-30 03:40:31 +0100

    Fix typo

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
             <div class="center">
                 <p>JS-Tricks.com provides tips, tricks, and techniques on JavaScript running on node.js and the browser in the form of snippets and tutorials.</p>
                 <p>It’s brought to you by <a href="https://fapprik.com/" id="fapprik">fapprik</a>, an agency from Munich which designs and develops JavaScript-powered apps for Facebook, smartphones, tablet PCs, smartwatches, smart TVs &amp; the Web.</p>
-                <p><a href="https://github.com/fapprik/js-tricks.com" class="github"><span>GitHub</span></a>It you’d like to contribute to this site, simply head over to our <a href="https://github.com/fapprik/js-tricks.com">GitHub repository</a>, fork it, make your contributions, and send a pull request.</p>
+                <p><a href="https://github.com/fapprik/js-tricks.com" class="github"><span>GitHub</span></a> If you’d like to contribute to this site, simply head over to our <a href="https://github.com/fapprik/js-tricks.com">GitHub repository</a>, fork it, make your contributions, and send a pull request.</p>
                 <p>© 2014 JS-Tricks.com · <a href="/contributors">Contributors</a> · <span class="obfuscated" data-content="Ask us your JavaScript questions!">ask AT js-tricks DOT com</span>
             </div>
         </footer>
