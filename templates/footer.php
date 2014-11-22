7e9c2556bed7773d0c33c42fce70556fe02f6692
Felix Zandanel <felix@zandanel.me>
2014-11-22 23:05:01 +0100

    Update analytics

--- templates/footer.php
+++ templates/footer.php
@@ -24,6 +24,7 @@
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             ga('create', 'UA-56248950-1', 'auto');
+            ga('require', 'displayfeatures');
             ga('set', 'anonymizeIp', true);
             ga('send', 'pageview');
         </script>
