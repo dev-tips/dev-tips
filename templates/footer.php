880def47698246953f0721cc91759df17a499903
Felix Zandanel <felix@zandanel.me>
2015-03-27 00:51:58 +0100

    Make copyright year in the footer dynamic

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
                 <p>It’s brought to you by <a href="https://fapprik.com/" id="fapprik">fapprik</a>, an agency from Munich which designs and develops JavaScript-powered apps for Facebook, smartphones, tablet PCs, smartwatches, smart TVs &amp; the Web.</p>
                 <p class="github"><a href="https://github.com/fapprik/js-tricks.com">GitHub</a> If you’d like to contribute to this site, simply head over to our <a href="https://github.com/fapprik/js-tricks.com">GitHub repository</a>, fork it, make your contributions, and send a pull request.</p>
                 <p>All articles are licensed under <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA 3.0</a>.</p>
-                <p>© 2014 <a href="https://fapprik.com/" id="fapprik-copyright">fapprik</a> · <a href="/contributors">Contributors</a> · <span class="obfuscated" data-content="Ask us your JavaScript questions!">ask AT js-tricks DOT com</span>
+                <p>© <?php echo date('Y') ?> <a href="https://fapprik.com/" id="fapprik-copyright">fapprik</a> · <a href="/contributors">Contributors</a> · <span class="obfuscated" data-content="Ask us your JavaScript questions!">ask AT js-tricks DOT com</span>
             </div>
         </footer>
         <div id="ribbon"><a href="https://github.com/fapprik/js-tricks.com">Fork me on GitHub</a></div>
