70a4d7a8936ec81a666e35f12a8c50f2c9af4c22
Thomas Rasshofer <hello@thomasrasshofer.com>
2017-08-15 19:30:28 +0200

    Implement error logger

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,35 @@
 
   const lazyLoader = new LazyLoad();
 
+  const originalErrorHandler = window.onerror;
+
+  const reportError = (body) => {
+    if (window.ga) {
+      ga('send', 'exception', {
+        exDescription: Object.keys(body).map((key) => `${key} = ${JSON.stringify(body[key])}`).join(' | ')
+      });
+    }
+  };
+
+  window.onerror = (errorMessage, url, lineNumber, columnNumber, errorObject) => {
+
+    reportError({
+      type: 'Error',
+      message: errorMessage,
+      url: url,
+      line: lineNumber,
+      column: columnNumber,
+      error: errorObject
+    });
+
+    if (typeof originalErrorHandler === 'function') {
+      return originalErrorHandler(errorMessage, url, lineNumber, columnNumber, errorObject);
+    }
+
+    return false;
+
+  };
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
