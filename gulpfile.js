8cd6bd1a306ce46b000dcf46197155d40ec7e3aa
Thomas Rasshofer <hello@thomasrasshofer.com>
2019-02-24 21:55:21 +0100

    Implement linting

--- gulpfile.js
+++ gulpfile.js
@@ -21,62 +21,59 @@ const content = path.resolve(src, 'content');
 const templates = path.resolve(src, 'templates');
 const output = path.resolve(__dirname, 'build');
 
-gulp.task('html', () => {
-  return gulp.src(path.resolve(output, '**/*.html')).pipe(minifyHtml({
-    removeComments: true,
-    collapseWhitespace: true,
-    removeAttributeQuotes: true
-  })).pipe(gulp.dest(output));
-});
+gulp.task('html', () => gulp.src(path.resolve(output, '**/*.html')).pipe(minifyHtml({
+  removeComments: true,
+  collapseWhitespace: true,
+  removeAttributeQuotes: true,
+})).pipe(gulp.dest(output)));
 
-gulp.task('css', () => {
-  return gulp.src(path.resolve(src, 'scss', 'style.scss')).pipe(sass({
-    importer: importOnce
-  })).pipe(autoprefixer({
-    browsers: [
-      'last 2 versions'
-    ]
-  })).pipe(groupMediaQueries()).pipe(cleanCss()).pipe(gulp.dest(path.resolve(output, 'css')));
-});
+gulp.task('css', () => gulp.src(path.resolve(src, 'scss', 'style.scss')).pipe(sass({
+  importer: importOnce,
+})).pipe(autoprefixer({
+  browsers: [
+    'last 2 versions',
+  ],
+})).pipe(groupMediaQueries())
+  .pipe(cleanCss())
+  .pipe(gulp.dest(path.resolve(output, 'css'))));
 
-gulp.task('js', () => {
-  return gulp.src([
-    path.resolve(vendor, 'vanilla-lazyload', 'dist', 'lazyload.js'),
-    path.resolve(src, 'js', 'scripts.js')
-  ]).pipe(babel()).pipe(uglify()).pipe(concat('scripts.js')).pipe(gulp.dest(path.resolve(output, 'js')));
-});
+gulp.task('js', () => gulp.src([
+  path.resolve(vendor, 'vanilla-lazyload', 'dist', 'lazyload.js'),
+  path.resolve(src, 'js', 'scripts.js'),
+]).pipe(babel()).pipe(uglify()).pipe(concat('scripts.js'))
+  .pipe(gulp.dest(path.resolve(output, 'js'))));
 
 gulp.task('cms', () => cms({
-  permalink: (permalink) => `${permalink}/`,
+  permalink: permalink => `${permalink}/`,
   template: consolidate.pug,
   paths: {
-    content: content,
-    templates: templates,
-    output: output
+    content,
+    templates,
+    output,
   },
   extensions: {
     templates: [
-      'pug'
+      'pug',
     ],
     images: [
       'jpg',
       'jpeg',
       'gif',
       'png',
-      'svg'
-    ]
+      'svg',
+    ],
   },
   addons: {
-    markdown: (input) => markdown.render(input),
+    markdown: input => markdown.render(input),
     formatDate: (timestamp, pattern) => moment(timestamp, 'X').format(pattern),
-    stripTags: (input) => striptags(input)
+    stripTags: input => striptags(input),
   },
   globals: {
-    site: 'Frontend Development'
+    site: 'Frontend Development',
   },
   shortcodes: {
     image: (attrs, page) => {
-      const image = page.images.find((item) => item.url.endsWith(`/${attrs.image}`));
+      const image = page.images.find(item => item.url.endsWith(`/${attrs.image}`));
       if (image) {
         const width = attrs.width || image.width;
         const height = attrs.height || image.height;
@@ -85,35 +82,31 @@ gulp.task('cms', () => cms({
           modifiers.push('bordered');
         }
         return `
-          <span class="image${modifiers.length ? ` ${modifiers.map((modifier) => `image--${modifier}`).join(' ')}`: ''}">
+          <span class="image${modifiers.length ? ` ${modifiers.map(modifier => `image--${modifier}`).join(' ')}` : ''}">
             <img data-src="${image.url}" alt="${attrs.title || image.title || image.alt || ''}"${width ? ` width="${width || ''}"` : ''}${height ? ` height="${height || ''}"` : ''}${attrs.title ? ` title="${attrs.title || ''}"` : ''}>
           </span>
         `;
-      } else {
-        throw new Error(`Missing image: ${attrs.image}`);
       }
+      throw new Error(`Missing image: ${attrs.image}`);
     },
     reference: (attrs, page) => {
-      const reference = page.genesis.children.find((child) => child.visible && child.index && child.index === parseInt(attrs.reference, 10));
+      const reference = page.genesis.children.find(child => child.visible && child.index && child.index === parseInt(attrs.reference, 10));
       if (reference) {
         return `<a href="${reference.url}">${attrs.text}</a>`;
-      } else {
-        throw new Error(`Missing reference: ${attrs.reference}`);
       }
+      throw new Error(`Missing reference: ${attrs.reference}`);
     },
-    math: (attrs) => `<span class="math">${attrs.math.replace(/\|LEFT_PARENTHESIS\|/g, '(').replace(/\|RIGHT_PARENTHESIS\|/g, ')')}</span>`,
-  }
+    math: attrs => `<span class="math">${attrs.math.replace(/\|LEFT_PARENTHESIS\|/g, '(').replace(/\|RIGHT_PARENTHESIS\|/g, ')')}</span>`,
+  },
 }).render());
 
 gulp.task('content', gulp.series('cms', 'html'));
 
-gulp.task('assets', () => {
-  return Promise.all([
-    gulp.src(path.resolve(src, 'favicon.ico')).pipe(gulp.dest(output)),
-    gulp.src(path.resolve(src, 'CNAME')).pipe(gulp.dest(output)),
-    gulp.src(path.resolve(src, 'img', '**', '*')).pipe(gulp.dest(path.resolve(output, 'img'))),
-  ]);
-});
+gulp.task('assets', () => Promise.all([
+  gulp.src(path.resolve(src, 'favicon.ico')).pipe(gulp.dest(output)),
+  gulp.src(path.resolve(src, 'CNAME')).pipe(gulp.dest(output)),
+  gulp.src(path.resolve(src, 'img', '**', '*')).pipe(gulp.dest(path.resolve(output, 'img'))),
+]));
 
 gulp.task('build', gulp.parallel('content', 'css', 'js', 'assets'));
 
@@ -123,6 +116,6 @@ gulp.task('dev', gulp.series('build'), () => {
     path.resolve(templates, '**/*'),
     path.resolve(src, 'scss', '**/*'),
     path.resolve(src, 'components', '**/*'),
-    path.resolve(src, 'js', '**/*')
+    path.resolve(src, 'js', '**/*'),
   ], gulp.series('build'));
 });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,26 +1,24 @@
 (() => {
-
-  const lazyLoader = new LazyLoad();
+  const lazyLoader = new LazyLoad(); // eslint-disable-line no-unused-vars
 
   const originalErrorHandler = window.onerror;
 
   const reportError = (body) => {
     if (window.ga) {
       ga('send', 'exception', {
-        exDescription: Object.keys(body).map((key) => `${key} = ${JSON.stringify(body[key])}`).join(' | ')
+        exDescription: Object.keys(body).map(key => `${key} = ${JSON.stringify(body[key])}`).join(' | '),
       });
     }
   };
 
   window.onerror = (errorMessage, url, lineNumber, columnNumber, errorObject) => {
-
     reportError({
       type: 'Error',
       message: errorMessage,
-      url: url,
+      url,
       line: lineNumber,
       column: columnNumber,
-      error: errorObject
+      error: errorObject,
     });
 
     if (typeof originalErrorHandler === 'function') {
@@ -28,9 +26,9 @@
     }
 
     return false;
-
   };
 
+  /* eslint-disable */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -39,5 +37,5 @@
   ga('create', 'UA-97110317-1', 'auto');
   ga('set', 'anonymizeIp', true);
   ga('send', 'pageview');
-
+  /* eslint-enable */
 })();
